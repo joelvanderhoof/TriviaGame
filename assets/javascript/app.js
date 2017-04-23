c897046639d0bea3fa782fec1c936cae881db60a
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2017-04-22 23:33:28 -0700

    Added a timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,27 +3,27 @@
 //Declare global variables
 var questions = [
 	{
-		"question": "?",
+		"question": "What is the answer?",
 		"choices": ["choice1", "choice2", "answer", "choice4", "choice5" ],
 		"answer": "answer"
 	},
 	{
-		"question": "?",
+		"question": "What is the answer?",
 		"choices": ["answer", "choice2", "choice3", "choice4", "choice5" ],
 		"answer": "answer"
 	},
 	{
-		"question": "?",
+		"question": "What is the answer?",
 		"choices": ["choice1", "choice2", "choice3", "answer", "choice5" ],
 		"answer": "answer"
 	},
 	{
-		"question": "?",
+		"question": "What is the answer?",
 		"choices": ["choice1", "choice2", "choice3", "choice4", "answer" ],
 		"answer": "answer"
 	},
 	{
-		"question": "?",
+		"question": "What is the answer?",
 		"choices": ["choice1", "answer", "choice3", "choice4", "choice5" ],
 		"answer": "answer"
 	}
@@ -33,7 +33,9 @@ var playerGuess;
 var timeRemaining;
 var guessedWrong = 0;
 var guessedRight = 0;
+var unanswered = 0;
 var counter = 0;
+var intervalId;
 
 
 //Declare functions
@@ -50,10 +52,114 @@ function showQuestion () {
 	$(".answer").html("<p>" + questions[counter].answer + "</p>");
 }
 
-// //Show the next question
-// function showNextQuestion() {
+// set a 10 second timer for each question
+var timer = {
 
-// }
+  	time: 10,
+
+  	reset: function() {
+
+	   	timer.time = 10;
+
+	   	//  TODO: Change the "display" div to "00:00."
+	   	$("#timer").html("10");
+  	},
+
+  	start: function () {
+
+   	 	//  Use setInterval to start the timer
+    	intervalId = setInterval(timer.count, 1000);
+  	},
+
+	count: function () {
+	 	// Count the timer down
+	 	timer.time--;
+	 	$("#timer").html(timer.time);
+ 	},
+
+ 	stop: function () {
+ 		clearInterval(intervalId);
+ 	}
+ };
+
+function updateStuff () {
+		counter++;
+		timer.reset();
+	console.log("guessed right: " + guessedRight);
+	console.log("guessed wrong: " + guessedWrong);
+	if (counter >= questions.length) {
+		alert("You won!!! New game?");
+		counter = 0;
+	}
+}
+
+function checkTimeRemaining() {
+
+	if (timer.time < 1) {
+				// If the player chooses the wrong answer,
+		// The scenario is similar for wrong answers and time-outs.
+		console.log("Ran out of time!");
+		unanswered++;
+		//tell the player they selected the wrong option 
+
+		//and then display the correct answer. 
+
+		//After a few seconds, display the next question -- do this without user input.
+		updateStuff();
+	}
+}
+
+
+//show the first question
+
+timer.start();
+showQuestion();
+checkTimeRemaining();
+
+
+//when the user clicks a choice, +1 to counter
+$(".choice").on("click", function() {
+	
+//check the answer
+	
+		console.log(this);
+		playerGuess = $(this).html();
+	
+	if (playerGuess === questions[counter].answer) {
+		//show a screen congratulating them for choosing the right option. 
+		console.log("right!" + questions[counter].answer);
+		guessedRight++;
+		//After a few seconds, display the next question -- do this without user input.
+		updateStuff();
+	} else if (playerGuess != questions[counter].answer) {
+		// If the player chooses the wrong answer,
+		// The scenario is similar for wrong answers and time-outs.
+		console.log("wrong!");
+		guessedWrong++;
+		//tell the player they selected the wrong option 
+
+		//and then display the correct answer. 
+
+		//After a few seconds, display the next question -- do this without user input.
+		updateStuff();
+	} else if (timer.time < 1) {
+				// If the player chooses the wrong answer,
+		// The scenario is similar for wrong answers and time-outs.
+		console.log("Ran out of time!");
+		unanswered++;
+		//tell the player they selected the wrong option 
+
+		//and then display the correct answer. 
+
+		//After a few seconds, display the next question -- do this without user input.
+		updateStuff();
+	}
+
+		updateStuff();
+		showQuestion();
+	});
+
+
 
 // //Execute these steps if the player guesses right
 // function guessedRight() {
@@ -65,7 +171,27 @@ function showQuestion () {
 
 // }
 
-//Check the answer
+
+
+ 
+
+
+
+		//Wait a few seconds, then show the next question.
+
+
+// On the final screen, 
+
+	//show the number of correct answers, 
+
+	//incorrect answers, 
+
+	//and an option to restart the game (without reloading the page).
+
+//---------------------------------------------------------------------
+
+
+	//Check the answer
 // function checkAnswer () {
 
 // 	// If the player runs out of time, 
@@ -97,61 +223,3 @@ function showQuestion () {
 
 // 		//After a few seconds, display the next question -- do this without user input.
 // 	}
-
-//show the first question
-showQuestion();
-
-//when the user clicks a choice, +1 to counter
-$(".choice").on("click", function() {
-	
-//check the answer
-	
-		console.log(this);
-		playerGuess = $(this).html();
-	
-	if (playerGuess === questions[counter].answer) {
-		//show a screen congratulating them for choosing the right option. 
-		console.log("right!" + questions[counter].answer);
-		guessedRight++;
-		//After a few seconds, display the next question -- do this without user input.
-
-	} else {
-		// If the player chooses the wrong answer,
-		// The scenario is similar for wrong answers and time-outs.
-		console.log("wrong!");
-		guessedWrong++;
-		//tell the player they selected the wrong option 
-
-		//and then display the correct answer. 
-
-		//After a few seconds, display the next question -- do this without user input.
-	}
-	counter++;
-	console.log("guessed right: " + guessedRight);
-	console.log("guessed wrong: " + guessedWrong);
-	if (counter >= questions.length) {
-		alert("You won!!! New game?");
-		counter = 0;
-	}
-		showQuestion();
-	});
-
-
-
-
-
-
- 
-
-
-
-		//Wait a few seconds, then show the next question.
-
-
-// On the final screen, 
-
-	//show the number of correct answers, 
-
-	//incorrect answers, 
-
-	//and an option to restart the game (without reloading the page).
